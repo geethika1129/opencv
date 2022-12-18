15a5b5b60a9733af8273a031ef75756cc6c5a0f4
Geethika Singareddy <geethika1129@gmail.com>
2022-12-18 11:31:00 +0530

    initial commit

--- intr0.py
+++ intr0.py
@@ -1,6 +1,15 @@
 import cv2
 print("package imported")
+
 #read images,vedios,webcam
-img=cv2.imread("Resources/image.png")
-cv2.imshow("output",img)
-cv2.waitKey(0)
+# img=cv2.imread("Resources/image.png")
+# cv2.imshow("output",img)
+# cv2.waitKey(0)
+
+#importing vedios
+cap=cv2.VideoCapture("Resources/banana.mp4")
+while True:
+    success ,img=cap.read()  #shows true or false
+    cv2.imshow("video",img)
+    if cv2.waitKey(1)&0xFF==ord('q'):#adds delay
+        break #if key q is pressed it will break
