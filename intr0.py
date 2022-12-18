66a761031b89f5c176338dad3d305afca105880f
Geethika Singareddy <geethika1129@gmail.com>
2022-12-18 12:19:37 +0530

    initial commit

--- intr0.py
+++ intr0.py
@@ -51,3 +51,14 @@ print("package imported")
 # #cv2.imshow("output",img)
 # cv2.imshow("output",imgcrop)
 # cv2.waitKey(0)
+
+img=np.zeros((512,512,3),np.uint8)
+print(img.shape)
+#img[:]=255,0,0 #colors entire blue
+# cv2.imshow("output",img)
+# cv2.waitKey(0)
+
+#creating lines
+cv2.line(img,(0,0),(300,300),(0,255,0),3)
+cv2.imshow("output",img)
+cv2.waitKey(0)
