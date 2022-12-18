65de1760c6aeab456976cb5a8ecaadb61eba6e74
Geethika Singareddy <geethika1129@gmail.com>
2022-12-18 11:55:44 +0530

    initial commit

--- intr0.py
+++ intr0.py
@@ -26,3 +26,9 @@ print("package imported")
 #     cv2.imshow("video",img)
 #     if cv2.waitKey(1) & 0xFF==ord('q'):#adds delay
 #        break #if key q is pressed it will break
+
+#basic functions
+img = cv2.imread("Resources/image.png")
+imggrey=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray Image",imggrey)
+cv2.waitKey(0)
