b61cd2d2847774a730ef0c52275b8aead80bb91a
Geethika Singareddy <geethika1129@gmail.com>
2022-12-19 10:18:56 +0530

    initial commit

--- intr0.py
+++ intr0.py
@@ -84,5 +84,11 @@ print("package imported")
 # cv2.waitKey(0)
 
 #face detection
-facecascade=cv2.CascadeClassifier()
-img=cv2.imread('Resources/')
+facecascade=cv2.CascadeClassifier("Resources/haarcascade_frontalface_default.xml")
+img=cv2.imread('Resources/face.jpg')
+imggray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+faces=facecascade.detectMultiScale(imggray,1.1,4)
+for(x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+cv2.imshow("result",img)
+cv2.waitKey(0)
